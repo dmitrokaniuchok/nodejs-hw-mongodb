05e159def1c46f38fe815e897f88b2050d014041
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-07-13 12:45:06 +0300

    fixed err

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,8 @@
 export function errorHandler(err, req, res, next) {
   console.error(err.stack);
 
-  res.status(500).json({
-    status: 500,
+  res.status(err.status || 500).json({
+    status: err.status || 500,
     message: 'Something went wrong',
     data: err.message,
   });

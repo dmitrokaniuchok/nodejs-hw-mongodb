cdc5b6c15b36c7b942b9bd7a827489ab6ad0bba0
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-07-27 11:25:48 +0300

    fix err

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,31 +1,39 @@
 import jwt from 'jsonwebtoken';
 import createHttpError from 'http-errors';
+import { Session } from '../models/Session.js';
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization || '';
 
-    if (!authHeader) {
-      throw createHttpError(401, 'Authorization header missing');
+    if (!authHeader.startsWith('Bearer ')) {
+      throw createHttpError(
+        401,
+        'Authorization header missing or invalid format',
+      );
     }
 
-    const [type, token] = authHeader.split(' ');
+    const token = authHeader.split(' ')[1];
 
-    if (type !== 'Bearer' || !token) {
-      throw createHttpError(401, 'Invalid authorization format');
-    }
+    let payload;
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-      if (err) {
-        if (err.name === 'TokenExpiredError') {
-          throw createHttpError(401, 'Access token expired');
-        }
-        throw createHttpError(401, 'Invalid access token');
+    try {
+      payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        throw createHttpError(401, 'Access token expired');
       }
+      throw createHttpError(401, 'Invalid access token');
+    }
 
-      req.user = { _id: payload.userId };
-      next();
-    });
+    const session = await Session.findOne({ accessToken: token });
+
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    req.user = { _id: payload.userId };
+    next();
   } catch (error) {
     next(error);
   }

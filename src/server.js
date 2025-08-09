a85527585930151eb9919fbb8bb7a69bf921157f
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-08-09 15:31:52 +0300

    add work

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const PORT = process.env.PORT || 3000;
 
 export function setupServer() {
@@ -18,6 +23,14 @@ export function setupServer() {
   app.use(pino());
   app.use(express.json());
   app.use(cookieParser());
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf-8'),
+  );
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
